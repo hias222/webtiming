c9f9ce3391eea9429e89b0d3e9c7806cca6511dd
Matthias Fuchs <matthias.fuchs@devtops.gmbh>
2019-08-18 22:26:17 +0200

    build

--- react_frontend/swimfrontend/src/components/lane.js
+++ react_frontend/swimfrontend/src/components/lane.js
@@ -1,20 +1,11 @@
 import React from 'react'
 import TableCell from '@material-ui/core/TableCell';
 import TableRow from '@material-ui/core/TableRow';
-import SvgIcon from '@material-ui/core/SvgIcon';
 import PoolIcon from '@material-ui/icons/Pool';
 
 import Typography from '@material-ui/core/Typography';
 
 import { styled } from '@material-ui/styles';
-
-function HomeIcon(props) {
-    return (
-      <SvgIcon {...props}>
-        <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
-      </SvgIcon>
-    );
-  }
 
 const NormalTableCell = styled(TableCell) ({
     fontSize: '1.2em'
@@ -28,19 +19,15 @@ class Lane extends React.Component {
     //const Service = ({ service }) => {
 
     render() {
-        var { place } = ""
         var {myicon} = ""
         //&& this.props.lane.place !== null
         if (this.props.lane.place !== 'undefined') {
             if (this.props.lane.place === "0") {
-                place = "<-->"
                 myicon = <PoolIcon/>
             } else {
-                place = this.props.lane.place
                 myicon = this.props.lane.place
             }
         } else {
-            place = "-"
             myicon = "-"
         }
 
