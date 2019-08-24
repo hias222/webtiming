8d205330095dc2fcf08f4d174c560923aad9a947
Matthias Fuchs <matthias.fuchs@devtops.gmbh>
2019-08-24 11:47:38 +0200

    update script

--- scripting/create_apps.sh
+++ scripting/create_apps.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+RASP_DIR=/Users/matthiasfuchs/projects/schwimmen/webtiming/raspberry/pi-gen
+
 START_DIR=`pwd`
 echo $START_DIR
 
@@ -8,17 +10,17 @@ echo react
 cd ../react_frontend/swimfrontend
 npm run-script build
 cd build
-rm ../../../../raspberry/pi-gen/stage6/02-install-web/files/web.zip
-zip ../../../../raspberry/pi-gen/stage6/02-install-web/files/web.zip * -r
+rm ${RASP_DIR}/stage6/02-install-web/files/web.zip
+zip ${RASP_DIR}/stage6/02-install-web/files/web.zip * -r
 
 echo datamapping
 cd $START_DIR
 cd ../datamapping
-rm ../../raspberry/pi-gen/stage6/01-install-app/files/app.zip
-zip ../../raspberry/pi-gen/stage6/01-install-app/files/app.zip app.js package.json incoming/* mqtt/* data/* resources/* views/*
+rm ${RASP_DIR}/stage6/01-install-app/files/app.zip
+zip ${RASP_DIR}/stage6/01-install-app/files/app.zip app.js package.json incoming/* mqtt/* data/* resources/* views/*
 
 echo websocket
 cd $START_DIR
 cd ../websocket_backend
-rm ../../raspberry/pi-gen/stage6/02-install-web/files/app.zip
-zip ../../raspberry/pi-gen/stage6/02-install-web/files/app.zip app.js package.json routes/* 
+rm ${RASP_DIR}/stage6/02-install-web/files/app.zip
+zip ${RASP_DIR}/stage6/02-install-web/files/app.zip app.js package.json routes/* 
