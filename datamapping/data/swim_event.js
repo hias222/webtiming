34e1b6289d4f57322aa6d7ae06e98826b9ac860a
Matthias Fuchs <matthias.fuchs@devtops.gmbh>
2019-10-02 13:11:05 +0200

    added relay message

--- datamapping/data/swim_event.js
+++ datamapping/data/swim_event.js
@@ -17,6 +17,7 @@ var event_all = null
 var event_sessions = null;
 var event_heatid = null;
 var event_swimmer = null;
+var event_relay = null;
 var event_clubs = null;
 
 var event_type = properties.get("main.event_type")
@@ -24,6 +25,7 @@ var event_type = properties.get("main.event_type")
 var internalheadID = "1";
 var actual_heat = "1";
 var actual_event = "1";
+var relay_count = "1";
 
 const event_type_values = {
     FINALE: 'FIN',
@@ -115,50 +117,56 @@ class swimevent {
             } else {
                 console.log("<swim_events> mode " + event_type)
                 var searchstring = "[?ATTR.number == '" + eventnumber + "' && ATTR.round == '" + event_type + "'].HEATS[*].HEAT[*]"
-                var myarray = jmespath.search(event_heatid, searchstring);
+                //var myarray = jmespath.search(event_heatid, searchstring);
                 var tmp_heats = jmespath.search(event_heatid, searchstring)[0][0];
-                
                 var searchheat = "[?ATTR.number == '" + heatnumber + "'].ATTR.heatid"
                 var newid = jmespath.search(tmp_heats, searchheat).toString()
                 internalheadID = newid
-                console.log("<swim_event> number heats " + myarray.length + " " + newid)
+                console.log("<swim_event> number round heat " + newid)
                 return newid;
             }
         } catch (err) {
-            console.log("<swim_events> getInternalHeatId crash " + eventnumber + " " + heatnumber + " mode " + event_type  )
-            console.log(err)
+            console.log("<swim_events> getInternalHeatId crash " + eventnumber + " " + heatnumber + " mode " + event_type)
+            console.log("<swim_events> nothing found !!!")
+            //console.log(err)
             internalheadID = 0;
             return new Object();
         }
     }
 
     getEventName(number) {
-        var emptyevent = "{ \"type\": \"header\", \"event\": \"" + actual_event + "\", \"heat\": \"" + actual_heat + "\" }"
+        console.log("<swim_event> search for number " + number)
+        var emptyevent = "{ \"type\": \"header\", \"relaycount\": \"1\", \"event\": \"" + actual_event + "\", \"heat\": \"" + actual_heat + "\" }"
         try {
             var searchstring = "[?ATTR.number == '" + number + "']"
             var tmp = jmespath.search(event_sessions, searchstring);
             var attributsearch = "[].{event: ATTR.number, gender: ATTR.gender, round: ATTR.round, relaycount: SWIMSTYLE[0].ATTR.relaycount, swimstyle: SWIMSTYLE[0].ATTR.stroke, distance: SWIMSTYLE[0].ATTR.distance }"
             var searcharray = jmespath.search(tmp, attributsearch);
-            if (searcharray.length > 1  && event_type !== event_type_values.ALL) {
-                console.log("wir müssen nochmal filtern " + event_type)
+            if (searcharray.length > 1 && event_type !== event_type_values.ALL) {
+                console.log("<swim_event> wir müssen nochmal filtern " + event_type)
                 for (let eventnames of searcharray) {
                     if (eventnames.round.toString() === event_type) {
-                        console.log("<swim_evnet> geteventname found " + eventnames.round);
+                        console.log("<swim_event> geteventname found " + eventnames.round);
                         var eventdata = eventnames;
+                        relay_count = typeof eventdata !== "undefined" ? eventdata.relaycount : "1";
                         var eventresult = typeof eventdata !== "undefined" ? eventdata : JSON.parse(emptyevent);
-                        return { ...eventresult, ...JSON.parse(emptyevent), ...this.getCompetitionName() };
+                        return { ...JSON.parse(emptyevent), ...eventresult, ...this.getCompetitionName() };
                     }
                 }
             } else {
+                console.log("<swim_event> single event")
                 var eventdata = jmespath.search(tmp, attributsearch)[0];
+                relay_count = typeof eventdata !== "undefined" ? eventdata.relaycount : "1";
                 var eventresult = typeof eventdata !== "undefined" ? eventdata : JSON.parse(emptyevent);
-                return { ...eventresult, ...JSON.parse(emptyevent), ...this.getCompetitionName() };
+                return { ...JSON.parse(emptyevent), ...eventresult, ...this.getCompetitionName() };
             }
         } catch (err) {
+            console.log("<swim_event> eemptyevent")
             console.log(err)
             try {
                 return JSON.parse(emptyevent);
             } catch (err) {
+                console.log("<swim_event> error return nullevent")
                 var nullevent = "{\"type\":\"header\",\"event\":\"0\",\"heat\":\"0\"}"
                 return JSON.parse(nullevent);
             }
@@ -173,13 +181,15 @@ class swimevent {
             time + "\", \"heat\": \"" +
             actual_heat + "\" }"
         try {
-            var lastswimmers = this.getSwimmerHeat(internalheadID);
-            //console.log("<swim_event> getActualSwimmer + heatid " + internalheadID )
-            var searchstring = "[?lane == '" + lane + "']"
-            var tmp = jmespath.search(lastswimmers, searchstring);
-            var swimmer = typeof tmp[0] !== "undefined" ? tmp[0] : JSON.parse(emptylane);
+            var lastswimmer = this.getSwimmerHeat(internalheadID, lane);
+            console.log("<swim_event> lastswimmer")
+            console.log(lastswimmer)
+            //var searchstring = "[?lane == '" + lane + "']"
+            //var tmp = jmespath.search(lastswimmers, searchstring);
+            //[].relay[][].RELAYPOSITION[].ATTR
+            var swimmer = typeof lastswimmer[0] !== "undefined" ? lastswimmer[0] : JSON.parse(emptylane);
             if (typeof swimmer.athleteid !== "undefined") {
-                var club = this.getSwimmerClub(tmp[0].athleteid)
+                var club = this.getSwimmerClub(lastswimmer[0].athleteid)
                 return { ...swimmer, ...club[0], ...JSON.parse(emptylane) };
             } else {
                 return swimmer;
@@ -195,8 +205,32 @@ class swimevent {
         }
     }
 
-    getSwimmerHeat(internalHeatID) {
-        var searchstring = "[?ENTRIES[?ENTRY[?ATTR.heatid == '" + internalHeatID + "']]]"
+
+    getRelayHeat(internalHeatID, lane) {
+        console.log("<swim_event> get relay for internalheatid " + internalHeatID + " no relay lane " + lane)
+        console.log("<swim_event> relay")
+
+        var searchstring = "[?RELAYS[?RELAY[?ENTRIES[?ENTRY[?ATTR.heatid == '" + internalHeatID + "' && ATTR.lane == '" + lane + "' ]]]]]"
+        var tmp = jmespath.search(event_clubs, searchstring);
+
+        var all_entries_only = jmespath.search(event_clubs, "[].RELAYS[].RELAY[].ENTRIES[].ENTRY[?ATTR.heatid == '" + internalHeatID + "' && ATTR.lane == '" + lane + "']")
+        var single_entry = jmespath.search(all_entries_only, "[]")
+        var cleared_entry = jmespath.search(single_entry, "[].{entrytime: ATTR.entrytime, lane: ATTR.lane, RELAYPOSTIONS: RELAYPOSITIONS[].RELAYPOSITION}")
+
+        var get_club_Search = "[].{code: ATTR.code, name: ATTR.name}"
+        var club_per_lane = jmespath.search(tmp, get_club_Search);
+
+        var complete_entry = [{ ...club_per_lane[0], ...cleared_entry[0]}]
+        
+        //console.log(JSON.stringify(complete_entry))
+    
+        return complete_entry;
+
+    }
+
+    getSingleSwimmerHeat(internalHeatID, lane) {
+        console.log("<swim_event> get swimmer for internalheatid " + internalHeatID + " no relay lane " + lane)
+        var searchstring = "[?ENTRIES[?ENTRY[?ATTR.heatid == '" + internalHeatID + "']]] " // | [?ENTRIES[?ENTRY[?ATTR.lane == '" + lane + "']]]"
         var tmp = jmespath.search(event_swimmer, searchstring);
         //console.log(tmp[0].ENTRIES[0].ENTRY)
         //console.log("getswimmerheate number swimmers " +  tmp.length)
@@ -204,8 +238,22 @@ class swimevent {
             lane: ENTRIES[0].ENTRY[?ATTR.heatid == '" + internalHeatID + "'].ATTR.lane , \
             entrytime: ENTRIES[0].ENTRY[?ATTR.heatid == '" + internalHeatID + "'].ATTR.entrytime}"
         var tmp2 = jmespath.search(tmp, searchstring2);
+        
         var searchstring3 = "[].{athleteid: athleteid, birthdate: birthdate, firstname: firstname, lastname: lastname, lane: lane[0], entrytime: entrytime[0] }"
-        return jmespath.search(tmp2, searchstring3);
+        var tmp3 = jmespath.search(tmp2, searchstring3);
+
+        var searchstring4 = "[?lane == '" + lane + "']"
+        var tmp4 = jmespath.search(tmp3, searchstring4);
+        
+        return tmp4
+    }
+
+    getSwimmerHeat(internalHeatID, lane) {
+        if (relay_count === "1") {
+            return this.getSingleSwimmerHeat(internalHeatID, lane)
+        } else {
+            return this.getRelayHeat(internalHeatID, lane)
+        }
     }
 
     getSwimmerClub(swimmerid) {
