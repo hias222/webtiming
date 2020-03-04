a3360040442ca329a01747f664f0d83bee8b2b18
Matthias Fuchs <matthias.fuchs@devtops.gmbh>
2020-03-04 16:34:04 +0100

    error config.txt

--- raspberry/pi-gen/lcd7/01-install-lcd/01-run-chroot.sh
+++ raspberry/pi-gen/lcd7/01-install-lcd/01-run-chroot.sh
@@ -12,8 +12,8 @@ echo " start LCD35-show "
 # hdmi_cvt 1024 600 60 6 0 0 0
 # max_usb_current=1 
 
-echo "hdmi_group=2" >> ./boot/config
-echo "hdmi_mode=1" >> ./boot/config
-echo "hdmi_mode=87" >> ./boot/config
-echo "hdmi_cvt 1024 600 60 6 0 0 0" >> ./boot/config
-echo "max_usb_current=1" >> ./boot/config
+echo "hdmi_group=2" >> ./boot/config.txt
+echo "hdmi_mode=1" >> ./boot/config.txt
+echo "hdmi_mode=87" >> ./boot/config.txt
+echo "hdmi_cvt 1024 600 60 6 0 0 0" >> ./boot/config.txt
+echo "max_usb_current=1" >> ./boot/config.txt
