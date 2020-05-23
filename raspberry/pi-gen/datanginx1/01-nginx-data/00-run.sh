515ebe9dfdafdcd597b7f84f157c1a157542417e
Matthias Fuchs <matthias.fuchs@devtops.gmbh>
2020-05-23 19:31:53 +0200

    add downloads

--- raspberry/pi-gen/datanginx1/01-nginx-data/00-run.sh
+++ raspberry/pi-gen/datanginx1/01-nginx-data/00-run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 
 install -v -d "${ROOTFS_DIR}/usr/share/nginx/html/data"
+install -v -d "${ROOTFS_DIR}/usr/share/nginx/html/download"
 
 #simpsons2.mp4
 install -v -m 644 files/video1.mp4 "${ROOTFS_DIR}/usr/share/nginx/html/data"
@@ -8,4 +9,10 @@ install -v -m 644 files/video2.mp4 "${ROOTFS_DIR}/usr/share/nginx/html/data"
 install -v -m 644 files/video3.mp4 "${ROOTFS_DIR}/usr/share/nginx/html/data"
 install -v -m 644 files/video4.mp4 "${ROOTFS_DIR}/usr/share/nginx/html/data"
 install -v -m 644 files/video5.mp4 "${ROOTFS_DIR}/usr/share/nginx/html/data"
+
+install -v -m 644 files/index.html "${ROOTFS_DIR}/usr/share/nginx/html/download"
+
+install -v -m 644 files/node-v12.16.3-x86.msi "${ROOTFS_DIR}/usr/share/nginx/html/download"
+install -v -m 644 files/nginx-1.17.10.zip "${ROOTFS_DIR}/usr/share/nginx/html/download"
+install -v -m 644 files/mosquitto-1.6.9-install-windows-x86.exe "${ROOTFS_DIR}/usr/share/nginx/html/download"
 
