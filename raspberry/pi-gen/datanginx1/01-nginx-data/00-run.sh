c1c22e1c5a0416665ea5a15cd0bbdea27384a89f
Matthias Fuchs <matthias.fuchs@devtops.gmbh>
2020-06-06 15:37:23 +0200

    add serial

--- raspberry/pi-gen/datanginx1/01-nginx-data/00-run.sh
+++ raspberry/pi-gen/datanginx1/01-nginx-data/00-run.sh
@@ -15,4 +15,4 @@ install -v -m 644 files/index.html "${ROOTFS_DIR}/usr/share/nginx/html/download"
 install -v -m 644 files/node-v12.16.3-x86.msi "${ROOTFS_DIR}/usr/share/nginx/html/download"
 install -v -m 644 files/nginx-1.17.10.zip "${ROOTFS_DIR}/usr/share/nginx/html/download"
 install -v -m 644 files/mosquitto-1.6.9-install-windows-x86.exe "${ROOTFS_DIR}/usr/share/nginx/html/download"
-
+install -v -m 644 files/serial.exe "${ROOTFS_DIR}/usr/share/nginx/html/download"
