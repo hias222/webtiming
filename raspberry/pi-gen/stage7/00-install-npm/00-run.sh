77dba4dcf4a1dba3c9936efb2e428efa91ddde12
Matthias <hias.fuchs@gmail.com>
2020-07-12 11:46:26 +0200

    update npm

--- raspberry/pi-gen/stage7/00-install-npm/00-run.sh
+++ raspberry/pi-gen/stage7/00-install-npm/00-run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-echo "copy node-v12.16.2-linux-armv7l.tar.xz"
-install -v -m 744 files/node-v12.16.2-linux-armv7l.tar.xz "${ROOTFS_DIR}/root"
+echo "copy node-v12.18.2-linux-armv7l.tar.xz"
+install -v -m 744 files/node-v12.18.2-linux-armv7l.tar.xz "${ROOTFS_DIR}/root"
 
 echo "end copy "
--- raspberry/pi-gen/stage7/00-install-npm/01-run-chroot.sh
+++ raspberry/pi-gen/stage7/00-install-npm/01-run-chroot.sh
@@ -1,11 +1,11 @@
 cd /root
 
-echo "install npm v12.16.2"
-tar -xf node-v12.16.2-linux-armv7l.tar.xz
+echo "install npm v12.18.2"
+tar -xf node-v12.18.2-linux-armv7l.tar.xz
 echo $PWD
 ls 
-rm node-v12.16.2-linux-armv7l.tar.xz
-cd node-v12.16.2-linux-armv7l
+rm node-v12.18.2-linux-armv7l.tar.xz
+cd node-v12.18.2-linux-armv7l
 echo $PWD
 cp -R * /usr/local/
 ls -la /usr/local/bin/npm | wc -l
